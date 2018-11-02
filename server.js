ee6a9de22b89a39a1fb195cc068165ec7fe219e1
sergeymaguire <sergeymaguire@gmail.com>
2018-11-01 20:55:33 -0700

    updates

--- server.js
+++ server.js
@@ -5,44 +5,60 @@ var express = require('express'),
 
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = 3000;
+
+HandleDataPArse();
 
 
-ParseJSONWithNPMBodyParser();
-
-
-
-GetReservationsAndTables();
+AJAXRoute();
 
 
 
-var tablesOfReservations = [
+var tables = [
   ];
 
 app.get('/api/tables', function (req, res) {
+
   res.json(tables);
 });
 
 
-const SeeIfBooked = BookLimitIsFive();
+const CheckForReservation = CheckReservation();
 
 
 DeleteReservations();
 
 
-function ParseJSONWithNPMBodyParser() {
+function HandleDataPArse() {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.text());
   app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 }
 
-function BookLimitIsFive() {
+function AJAXRoute() {
+  app.get('/', function (req, res) {
+    console.log('home page requested');
+    res.sendFile(path.join(__dirname, 'app/public/index.html'));
+  });
+  app.get('/tables', function (req, res) {
+    console.log('tables page requested');
+    res.sendFile(path.join(__dirname, 'app/public/tables.html'));
+  });
+  app.get('/reserve', function (req, res) {
+    console.log('reserve page requested');
+    res.sendFile(path.join(__dirname, 'app/public/reserve.html'));
+  });
+}
+
+function CheckReservation() {
   return app.post('/api/reserve', function (req, res) {
+    console.log('reserve request submitted');
+    console.log(req.body);
     var newReservation = req.body;
-    tablesOfReservations.push(newReservation);
+    tables.push(newReservation);
     var isBooked;
-    if (tablesOfReservations.length <= 5) {
+    if (tables.length <= 5) {
       isBooked = true;
     }
     else {
@@ -54,24 +70,14 @@ function BookLimitIsFive() {
 
 function DeleteReservations() {
   app.post('/api/clear', function (req, res) {
-    tablesOfReservations = [];
+    console.log('clear all tables');
+    tables = [];
     res.sendFile(path.join(__dirname, 'app/public/tables.html'));
   });
   app.post('/api/killreservation', function (req, res) {
+    console.log(req.body.id);
     tables.splice(req.body.id, 1);
     res.json(tables);
-  });
-}
-
-function GetReservationsAndTables() {
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'app/public/index.html'));
-  });
-  app.get('/tables', function (req, res) {
-    res.sendFile(path.join(__dirname, 'app/public/tables.html'));
-  });
-  app.get('/reserve', function (req, res) {
-    res.sendFile(path.join(__dirname, 'app/public/reserve.html'));
   });
 }
 
