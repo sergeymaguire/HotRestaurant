43e073025c0d502e0b6067e6cf97bd753f5dac97
sergeymaguire <sergeymaguire@gmail.com>
2018-11-01 20:05:05 -0700

    updates

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
-// Dependencies
-// =============================================================
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
 
-// Sets up the Express App
-// =============================================================
+var express = require('express'),
+ bodyParser = require('body-parser'),
+ path = require('path');
+
+
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
-// Routes
-// =============================================================
 
-// Basic route that sends the user first to the AJAX Page
 app.get('/', function (req, res) {
 	console.log('home page requested');
 	res.sendFile(path.join(__dirname, 'app/public/index.html'));
